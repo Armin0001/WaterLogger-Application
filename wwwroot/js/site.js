6c1df144bf943369a9258e0fa7e030496b0652e4
Armin0001 <59747818+Armin0001@users.noreply.github.com>
2023-01-18 09:43:01 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿function calculate() {
+    var tbl = document.getElementById("records");
+    var resultArea = document.getElementById("result");
+    var result = 0;
+
+    for (var i = 1; i < tbl.rows.length; i++) {
+        result = result + +tbl.rows[i].cells[1].innerHTML;
+    }
+
+    
+
+    const myTimeout = setTimeout(resultArea.append(`${result}`), 40);
+       
+
+    function myStopFunction() {
+        clearTimeout(myTimeout);
+    }
+
+    setTimeout(myStopFunction(), 3000);
+    
+}
